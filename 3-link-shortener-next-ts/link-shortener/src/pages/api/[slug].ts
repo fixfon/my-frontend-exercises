49523701f99601d52404a5bdb6b3c2f9e38d219e
fixfon <itsme@fixfon.dev>
2022-08-11 17:20:06 +0300

    Prisma with planetscale and first model with working redirection

---
+++ 3-link-shortener-next-ts/link-shortener/src/pages/api/[slug].ts
@@ -0,0 +1,32 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { prisma } from '../../db/client';
+
+const getURL = async (req: NextApiRequest, res: NextApiResponse) => {
+	const slug = req.query['slug'];
+
+	if (!slug || typeof slug !== 'string') {
+		res.statusCode = 404;
+		res.send(JSON.stringify({ message: 'use slug' }));
+
+		return;
+	}
+
+	const data = await prisma.shortLink.findFirst({
+		where: {
+			slug: {
+				equals: slug,
+			},
+		},
+	});
+
+	if (!data) {
+		res.statusCode = 404;
+		res.send(JSON.stringify({ message: 'slug not found' }));
+
+		return;
+	}
+
+	return res.redirect(data.url);
+};
+
+export default getURL;
