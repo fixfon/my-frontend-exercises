b5c5efd021b45331fd9c5228bb8d160d35cd5077
fixfon <itsme@fixfon.dev>
2022-08-11 18:00:06 +0300

    Added caching

--- 3-link-shortener-next-ts/link-shortener/src/pages/api/get-url/[slug].ts
+++ 3-link-shortener-next-ts/link-shortener/src/pages/api/get-url/[slug].ts
@@ -26,6 +26,11 @@ const getURL = async (req: NextApiRequest, res: NextApiResponse) => {
 		return;
 	}
 
+	// This is vercel caching. It is not browser caching.
+	res.setHeader('Content-Type', 'application/json');
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Cache-Control', 's-maxage=10000000, stale-while-revalidate');
+
 	return res.json(data);
 	// return res.redirect(data.url);
 };
