5acec34d85f6c30fd1a6710289ecf022bcf687c0
fixfon <itsme@fixfon.dev>
2022-08-12 13:42:45 +0300

    Added tRPC routing

--- 3-link-shortener-next-ts/link-shortener/src/pages/_app.tsx
+++ 3-link-shortener-next-ts/link-shortener/src/pages/_app.tsx
@@ -1,8 +1,29 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { withTRPC } from '@trpc/next';
+import { AppRouter } from './api/trpc/[trpc]';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	console.log('Hitted MyApp _app.tsx');
+	return <Component {...pageProps} />;
 }
 
-export default MyApp
+function getBaseUrl() {
+	console.log('Hitted getBaseUrl _app.tsx');
+	if (process.browser) return '';
+	if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+
+	return `https://localhost:${process.env.PORT ?? 3000}`;
+}
+
+export default withTRPC<AppRouter>({
+	config() {
+		console.log('Hitted withTRPC config _app.tsx');
+		const url = `${getBaseUrl()}/api/trpc`;
+
+		return {
+			url,
+		};
+	},
+	ssr: false,
+})(MyApp);
